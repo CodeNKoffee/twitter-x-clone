06cedf8fb887235a0e1725ac16f9fff24198ef87
Hatem Soliman <hatemthedev@gmail.com>
2024-05-02 21:20:15 +0300

    Added PostFeed and Trending backbone structure

---
+++ components/PostFeed.tsx
@@ -0,0 +1,9 @@
+export default function PostFeed() {
+  return (
+    <div className="sm:ml-20 xl:ml-96 max-w-2xl flex-grow border-x-gray-700">
+      <div className="sticky top-0 z-50 px-3 py-2 text-lg sm:text-xl font-bold border-b border-gray-700">
+        Home
+      </div>
+    </div>
+  )
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Sidebar from "@/components/Sidebar";
+import PostFeed from "@/components/PostFeed";
+import Trending from "@/components/Trending";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="bg-black max-w-[1400px] min-h-screen text-[#E7E9EA] mx-auto">
+    <div className="bg-black max-w-[1400px] min-h-screen text-[#E7E9EA] mx-auto flex">
       <Sidebar />
-      {/* <PostsFeed />
-      <Trending /> */}
+      <PostFeed />
+      <Trending />
     </div>
   );
 }
