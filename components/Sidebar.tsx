e676e4be89b6fc9580d4995087806fa8bd534f8b
Hatem Soliman <hatemthedev@gmail.com>
2024-05-03 21:01:55 +0300

    Updated sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,70 +1,82 @@
-import { sidebarLinks } from "@/constants";
-import { SidebarLinkProps } from "@/types";
-import { 
-  HomeIcon, 
+import { auth } from "@/firebase";
+import { handleLoginModal, handleSignupModal } from "@/redux/modalSlice";
+import { RootState } from "@/redux/store";
+import { signOutUser } from "@/redux/userSlice";
+import {
+  HomeIcon,
   HashtagIcon,
+  InboxIcon,
   BookmarkIcon,
   ClipboardListIcon,
+  BellIcon,
   UserIcon,
-  BellIcon, 
-  InboxIcon,
   DotsCircleHorizontalIcon,
   DotsHorizontalIcon,
 } from "@heroicons/react/outline";
+import { signOut } from "firebase/auth";
 import Image from "next/image";
-import TwitterLogo from "@/public/assets/twitter-logo.png";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Sidebar() {
+  const dispatch = useDispatch();
+
+  const user = useSelector((state: RootState) => state.user)
+
+  async function handleSignOut() {
+    await signOut(auth);
+    dispatch(signOutUser());
+    dispatch(handleSignupModal())
+    dispatch(handleLoginModal())
+  }
   return (
-    <div className="fixed hidden h-full md:flex flex-col">
-      <nav className="h-full marker:xl:space-y-1.5">
-        {/* {sidebarLinks.map((sidebarLink, index) => (
-          <SidebarLink
-            key={index}
-            text={sidebarLink.text}
-            Icon={sidebarLink.Icon}
-          />
-        )} */}
-        <div className="py-3 xl:p-3 flex xl:justify-start justify-center items-center">
-          <Image 
-            src={TwitterLogo}
-            alt="White Twitter Logo"
-            width={34}
-            height={34}
-          />
+    <div className="h-full hidden sm:flex flex-col fixed xl:ml-24">
+      <nav className="h-full relative xl:space-y-8">
+        <div className="flex justify-center xl:justify-start items-center py-3 xl:p-3">
+          <Image src={"/assets/twitter-logo.png"} alt="Twitter Logo" width={34} height={34} />
         </div>
-        <SidebarLink text="Home" Icon={HomeIcon} />
-        <SidebarLink text="Explore" Icon={HashtagIcon} />
-        <SidebarLink text="Notifications" Icon={BellIcon} />
-        <SidebarLink text="Messages" Icon={InboxIcon} />
-        <SidebarLink text="Bookmarks" Icon={BookmarkIcon} />
-        <SidebarLink text="Profile" Icon={UserIcon} />
-        <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
-        <button className="hidden xl:inline bg-[#1D9BF0] rounded-full h-[52px] mt-2 w-[200px] text-lg font-bold">
-          Tweet 
+        <SidebarLink Icon={HomeIcon} text={"Home"} />
+        <SidebarLink Icon={HashtagIcon} text={"Explore"} />
+        <SidebarLink Icon={BellIcon} text={"Notifications"} />
+        <SidebarLink Icon={InboxIcon} text={"Messages"} />
+        <SidebarLink Icon={BookmarkIcon} text={"Bookmarks"} />
+        <SidebarLink Icon={UserIcon} text={"Profile"} />
+        <SidebarLink Icon={DotsCircleHorizontalIcon} text={"More"} />
+        <button
+          className="hidden xl:inline bg-[#1d9bf0]
+        rounded-full h-[52px] mt-2 w-[200px] text-lg font-bold
+        "
+        >
+          Tweet
         </button>
-        <div className="absolute bottom-0 hover:bg-white hover:bg-opacity-10 flex justify-center items-center p-3 sapce-x-3 rounded-full cursor-pointer">
-          <img 
-            src="/assets/pfp.png" 
-            alt="Elon" 
-            className="rounded-full w-10 h-10 object-cover"
+        <div
+          onClick={handleSignOut}
+          className="
+        bottom-0
+        hover:bg-white hover:bg-opacity-10 rounded-full cursor-pointer
+        absolute xl:p-3 flex justify-center items-center  space-x-3"
+        >
+          <img
+            className="w-10 h-10 rounded-full
+          object-cover
+          "
+            src={user.photoUrl || "/assets/kylie.png" } 
           />
           <div className="hidden xl:inline">
-            <h1 className="font-bold whitespace-nowrap">name</h1>
-            <h1 className="text-gray-500">@username</h1>
+            <h1 className="font-bold whitespace-nowrap">{user.name}</h1>
+            <h1 className="text-gray-500">@{user.username}</h1>
           </div>
-          <DotsHorizontalIcon className="h-5" />
+          <DotsHorizontalIcon className="h-5 hidden xl:inline" />
         </div>
       </nav>
     </div>
-  )
-};
+  );
+}
 
-function SidebarLink({ text, Icon }: SidebarLinkProps) {
+function SidebarLink({ text, Icon }: any) {
   return (
-    <li className="hover__animation text-xl mb-6 flex xl:justify-start justify-center items-center space-x-3">
+    <li className="hoverAnimation flex mb-3 xl:justify-start justify-center items-center text-xl space-x-3">
       <Icon className="h-7" />
       <span className="hidden xl:inline">{text}</span>
     </li>
-  )
+  );
 }
