2d9851b6ec9193f6bc4a64dcdc73652f31633e4e
Hatem Soliman <hatemthedev@gmail.com>
2024-05-03 22:39:54 +0300

    Updated classes and icons

--- components/TweetInput.tsx
+++ components/TweetInput.tsx
@@ -9,7 +9,13 @@ import {
   PhotographIcon,
   XIcon,
 } from "@heroicons/react/outline";
-import { addDoc, collection, doc, serverTimestamp, updateDoc } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
 import { getDownloadURL, ref, uploadString } from "firebase/storage";
 import { useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -28,11 +34,11 @@ export default function TweetInput() {
 
   async function sendTweet() {
     if (!user.username) {
-      dispatch(handleLoginModal())
-      return
+      dispatch(handleLoginModal());
+      return;
     }
 
-    setLoading(true)
+    setLoading(true);
 
     const docRef = await addDoc(collection(db, "posts"), {
       username: user.username,
@@ -41,39 +47,39 @@ export default function TweetInput() {
       uid: user.uid,
       timestamp: serverTimestamp(),
       likes: [],
-      tweet: text
+      tweet: text,
     });
-
 
     if (image) {
       // Convert ArrayBuffer to Base64 string
-      const base64Image = typeof image === 'string' ? image : arrayBufferToBase64(image);
-  
+      const base64Image =
+        typeof image === "string" ? image : arrayBufferToBase64(image);
+
       const imageRef = ref(storage, `tweetImages/${docRef.id}`);
       const uploadImage = await uploadString(imageRef, base64Image, "data_url");
       const downloadURL = await getDownloadURL(imageRef);
       await updateDoc(doc(db, "posts", docRef.id), {
-        image: downloadURL
+        image: downloadURL,
       });
     }
 
-    setText("")
-    setImage(null)
-    setLoading(false)
+    setText("");
+    setImage(null);
+    setLoading(false);
   }
 
-  function addImagetoTweet(e: React.ChangeEvent<HTMLInputElement>){
-    const reader = new FileReader()
-    if (e.target.files && e.target.files[0]){
-      reader.readAsDataURL(e.target.files[0])
+  function addImagetoTweet(e: React.ChangeEvent<HTMLInputElement>) {
+    const reader = new FileReader();
+    if (e.target.files && e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
     }
 
-    reader.addEventListener("load", e => {
-      reader.addEventListener("load", e => {
+    reader.addEventListener("load", (e) => {
+      reader.addEventListener("load", (e) => {
         const image = e.target ? e.target.result : null;
         setImage(() => image);
       });
-    })
+    });
   }
 
   // Helper function to convert ArrayBuffer to Base64 string
@@ -95,71 +101,78 @@ export default function TweetInput() {
 
       {loading && <h1 className="text-2xl text-gray-500">Uploading post...</h1>}
 
-      {!loading && (<div className="w-full">
-        <textarea
-          placeholder="What's on your mind?"
-          className="bg-transparent resize-none outline-none w-full min-h-[50px] text-lg"
-          onChange={(e) => setText(e.target.value)}
-          value={text}
-        />
+      {!loading && (
+        <div className="w-full">
+          <textarea
+            placeholder="What's on your mind?"
+            className="bg-transparent resize-none outline-none w-full min-h-[50px] text-lg"
+            onChange={(e) => setText(e.target.value)}
+            value={text}
+          />
 
-        {image && (
-          <div className="relative mb-4">
-
-            <div
-            onClick={() => setImage(null)}
-            className="absolute top-1 left-1
+          {image && (
+            <div className="relative mb-4">
+              <div
+                onClick={() => setImage(null)}
+                className="absolute top-1 left-1
             bg-[#272c26] rounded-full w-8 h-8 flex justify-center
             items-center cursor-pointer hover:bg-white hover:bg-opacity-10
-            ">
-              <XIcon className="h-5" /> 
+            "
+              >
+                <XIcon className="h-5" />
+              </div>
+              <img
+                className="rounded-2xl max-h-80 object-contain"
+                src={typeof image === "string" ? image : ""}
+              />
+            </div>
+          )}
 
+          <div className="flex justify-between border-t border-gray-700 pt-4">
+            {/* ICNONS DIV */}
+            <div className="flex space-x-2">
+              <div
+                onClick={() =>
+                  filePickerRef.current
+                    ? filePickerRef.current.click()
+                    : console.log("")
+                }
+                className="icon__animation"
+              >
+                <PhotographIcon className="h-[22px] text-[#1d9bf0]" />
+              </div>
+              <input
+                ref={filePickerRef}
+                onChange={addImagetoTweet}
+                className="hidden"
+                type="file"
+              />
+              <div className="icon__animation">
+                <ChartBarIcon className="h-[22px] text-[#1d9bf0]" />
+              </div>
+              <div className="icon__animation">
+                <EmojiHappyIcon className="h-[22px] text-[#1d9bf0]" />
+              </div>
+              <div className="icon__animation">
+                <CalendarIcon className="h-[22px] text-[#1d9bf0]" />
+              </div>
+              <div className="icon__animation">
+                <LocationMarkerIcon className="h-[22px] text-[#1d9bf0]" />
+              </div>
             </div>
-            <img
-              className="rounded-2xl max-h-80 object-contain"
-              src={typeof image === 'string' ? image : ''}
-            />
 
-
-          </div>
-        )}
-
-        <div className="flex justify-between border-t border-gray-700 pt-4">
-          {/* ICNONS DIV */}
-          <div className="flex space-x-0">
-            <div
-            onClick={() => filePickerRef.current ? filePickerRef.current.click() : console.log("")}
-            className="iconAnimation">
-              <PhotographIcon className="h-[22px] text-[#1d9bf0]" />
-            </div>
-            <input 
-            ref={filePickerRef}
-            onChange={addImagetoTweet}
-            className="hidden" type="file" />
-            <div className="iconAnimation">
-              <ChartBarIcon className="h-[22px] text-[#1d9bf0]" />
-            </div>
-            <div className="iconAnimation">
-              <EmojiHappyIcon className="h-[22px] text-[#1d9bf0]" />
-            </div>
-            <div className="iconAnimation">
-              <CalendarIcon className="h-[22px] text-[#1d9bf0]" />
-            </div>
-            <div className="iconAnimation">
-              <LocationMarkerIcon className="h-[22px] text-[#1d9bf0]" />
-            </div>
-          </div>
-
-          <button
-          onClick={sendTweet}
-          disabled={!text && !image}
-          className="bg-[#1d9bf0] rounded-full px-4 py-1.5
+            <button
+              onClick={sendTweet}
+              disabled={!text && !image}
+              className="bg-[#1d9bf0] rounded-full px-4 py-1.5
           disabled:opacity-50
-          ">
-            Tweet
-          </button>
+          "
+            >
+              Tweet
+            </button>
+          </div>
         </div>
-      </div>)}
+      )}
     </div>
   );
 }
--- components/modals/Comment.tsx
+++ components/modals/Comment.tsx
@@ -16,40 +16,42 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function CoomentModal() {
-  const isOpen = useSelector((state: RootState) => state.modals.commentModalOpen);
+  const isOpen = useSelector(
+    (state: RootState) => state.modals.commentModalOpen
+  );
   const userImg = useSelector((state: RootState) => state.user.photoUrl);
-  const tweetDetails = useSelector((state: RootState) => state.modals.commentTweetDetails)
-  const user = useSelector((state: RootState) => state.user)
+  const tweetDetails = useSelector(
+    (state: RootState) => state.modals.commentTweetDetails
+  );
+  const user = useSelector((state: RootState) => state.user);
 
   const dispatch = useDispatch();
 
-  const [comment, setComment] = useState("")
+  const [comment, setComment] = useState("");
 
-  const router = useRouter()
+  const router = useRouter();
 
-  async function sendComment(){
+  async function sendComment() {
     if (!tweetDetails.id) {
       console.error("tweetDetails.id is null or undefined");
       return;
     }
-    
-    const docRef = doc(db, "posts", tweetDetails.id)
+
+    const docRef = doc(db, "posts", tweetDetails.id);
     const commentDetails = {
       username: user.username,
       name: user.name,
       photoUrl: user.photoUrl,
-      comment: comment
-    }
+      comment: comment,
+    };
     await updateDoc(docRef, {
-      comments: arrayUnion(commentDetails)
-    })
+      comments: arrayUnion(commentDetails),
+    });
 
-    dispatch(handleCommentModal())
-    router.push("/" + tweetDetails.id)
-
+    dispatch(handleCommentModal());
+    router.push("/" + tweetDetails.id);
   }
 
-  
   return (
     <>
       <Modal
@@ -64,13 +66,16 @@ export default function CoomentModal() {
         sm:p-10 p-4
         "
         >
-          <div className="absolute w-[2px] h-[77px] bg-gray-500
+          <div
+            className="absolute w-[2px] h-[77px] bg-gray-500
           left-[40px] top-[96px] sm:left-[64px] sm:top-[120px]
           
-          "></div>
+          "
+          ></div>
           <div
-          onClick={() => dispatch(handleCommentModal())}
-          className="absolute top-4 cursor-pointer" >
+            onClick={() => dispatch(handleCommentModal())}
+            className="absolute top-4 cursor-pointer"
+          >
             <XIcon className="w-6" />
           </div>
 
@@ -88,7 +93,10 @@ export default function CoomentModal() {
                 </div>
                 <p className="mt-1">{tweetDetails.tweet}</p>
                 <h1 className="text-gray-500 text-[15px] mt-2">
-                  Replying to <span className="text-[#1b9bf0]">@{tweetDetails.username}</span>
+                  Replying to{" "}
+                  <span className="text-[#1b9bf0]">
+                    @{tweetDetails.username}
+                  </span>
                 </h1>
               </div>
             </div>
@@ -108,24 +116,24 @@ export default function CoomentModal() {
                 bg-transparent resize-none
                 
                 "
-                onChange={e => setComment(e.target.value)}
+                  onChange={(e) => setComment(e.target.value)}
                 />
 
                 <div className="pt-4 flex justify-between border-t border-gray-700">
                   <div className="flex space-x-0">
-                    <div className="iconAnimation">
+                    <div className="icon__animation">
                       <PhotographIcon className="h-[22px] text-[#1d9bf0]" />
                     </div>
-                    <div className="iconAnimation">
+                    <div className="icon__animation">
                       <ChartBarIcon className="h-[22px] text-[#1d9bf0]" />
                     </div>
-                    <div className="iconAnimation">
+                    <div className="icon__animation">
                       <EmojiHappyIcon className="h-[22px] text-[#1d9bf0]" />
                     </div>
-                    <div className="iconAnimation">
+                    <div className="icon__animation">
                       <CalendarIcon className="h-[22px] text-[#1d9bf0]" />
                     </div>
-                    <div className="iconAnimation">
+                    <div className="icon__animation">
                       <LocationMarkerIcon className="h-[22px] text-[#1d9bf0]" />
                     </div>
                   </div>
@@ -133,8 +141,8 @@ export default function CoomentModal() {
                     className="bg-[#1d9bf0] rounded-full px-4 py-1.5
                   disabled:opacity-50
                   "
-                  disabled={!comment}
-                  onClick={sendComment}
+                    disabled={!comment}
+                    onClick={sendComment}
                   >
                     Tweet
                   </button>
