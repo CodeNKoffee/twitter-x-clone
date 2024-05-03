a0da22d01660a65a799e7814a5ca84dd56b59ab3
Hatem Soliman <hatemthedev@gmail.com>
2024-05-03 18:38:17 +0300

    Updated Signup

--- components/modals/SignUp.tsx
+++ components/modals/SignUp.tsx
@@ -1,52 +1,77 @@
-import { auth } from "@/firebase";
-import { handleSignupModal } from "@/redux/modalSlice";
-import { RootState } from "@/redux/store";
-import { setUser } from "@/redux/userSlice";
+import { handleLoginModal, handleSignupModal } from "@/redux/modalSlice";
 import Modal from "@mui/material/Modal";
-import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
-import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "@/firebase";
+import { useEffect, useState } from "react";
+import { setUser } from "@/redux/userSlice";
+import { useRouter } from "next/router";
+import { RootState } from "@/redux/store";
 
-export default function SignUp() {
-  const [fullname, setFullname] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-
+export default function SignupModal() {
   const isOpen = useSelector((state: RootState) => state.modals.signupModalOpen);
   const dispatch = useDispatch();
 
-  const handleSignUp = async () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter()
+
+  async function handleSingUp() {
     const userCredentials = await createUserWithEmailAndPassword(
-      auth, 
+      auth,
       email,
       password
-    )
+    );
+
+    if (auth.currentUser) {
+      await updateProfile(auth.currentUser, {
+        displayName: name,
+        photoURL: `./assets/profilePictures/pfp${Math.ceil(Math.random() * 6)}.png`
+  
+      })
+    }
+
+    router.reload()
+  
   }
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser ) => {
-      if (!currentUser) return
+  async function handleGuestSignIn(){
+    await signInWithEmailAndPassword(auth, "guest111100@gmail.com", "123456")
 
-      // Handle Redux actions
+  }
+
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (!currentUser) return;
+      console.log(currentUser);
       dispatch(
         setUser({
           username: currentUser.email ? currentUser.email.split("@")[0] : '',
-          name: null, 
-          email: currentUser.email, 
+          name: currentUser.displayName,
+          email: currentUser.email,
           uid: currentUser.uid,
-          photoUrl: null
+          photoUrl: currentUser.photoURL,
         })
-      )
-    })
+      );
+    });
 
     return unsubscribe;
-  }, [])
+  }, []);
 
   return (
     <>
-      <button 
-        className="rounded-full bg-white text-black w-[160px] h-[40px] hover:bg-[#CBD2D7]"
+      <button
+        className="bg-white text-black
+        w-[160px] rounded-full h-[40px] hover:bg-[#cbd2d7]
+        "
         onClick={() => dispatch(handleSignupModal())}
       >
         Sign Up
@@ -57,36 +82,46 @@ export default function SignUp() {
         onClose={() => dispatch(handleSignupModal())}
         className="flex justify-center items-center"
       >
-        <div className="w-[90%] h-[600px] bg-black text-white md:w-[600px] border border-gray-700 rounded-lg flex flex-col items-center">
+        <div
+          className="w-[90%] h-[600px] bg-black text-white md:w-[560px] md:h-[600px] border border-gray-700 rounded-lg
+        flex justify-center
+        "
+        >
           <div className="w-[90%] mt-8 flex flex-col">
-            <button 
-              className="bg-white text-black w-full font-bold text-lg p-2 rounded-md"
+            <button
+              className="bg-white text-black w-full font-bold
+                text-lg p-2 rounded-md
+                "
+                onClick={handleGuestSignIn}
             >
               Sign In as Guest
             </button>
             <h1 className="text-center mt-4 font-bold text-lg">or</h1>
-            <h1 className="mt-4 font-bold text-4xl text-center">Create your Account</h1>
-            <input 
-              className="h-10 rounded-md bg-transparent border border-gray-700 p-6 mt-8" 
-              placeholder="Full Name" 
-              type="text" 
-              onChange={(e) => setFullname(e.target.value)}
+            <h1 className="mt-4 font-bold text-4xl">Create your account</h1>
+            <input
+              placeholder="Full Name"
+              className="h-10 mt-8 rounded-md bg-transparent border border-gray-700 p-6"
+              type={"text"}
+              onChange={(e) => setName(e.target.value)}
             />
-            <input 
-              className="h-10 rounded-md bg-transparent border border-gray-700 p-6 mt-8" 
-              placeholder="Email" 
-              type="email" 
+            <input
+              placeholder="Email"
+              className="h-10 mt-8 rounded-md bg-transparent border border-gray-700 p-6"
+              type={"email"}
               onChange={(e) => setEmail(e.target.value)}
             />
-            <input 
-              className="h-10 rounded-md bg-transparent border border-gray-700 p-6 mt-8" 
-              placeholder="Password" 
-              type="password" 
+            <input
+              placeholder="Password"
+              className="h-10 mt-8 rounded-md bg-transparent border border-gray-700 p-6"
+              type={"password"}
               onChange={(e) => setPassword(e.target.value)}
             />
-            <button 
-              className="bg-white text-black w-full font-bold text-lg p-2 mt-8 rounded-md"
-              onClick={handleSignUp}
+
+            <button
+              className="bg-white text-black w-full font-bold
+                text-lg p-2 mt-8 rounded-md
+                "
+              onClick={handleSingUp}
             >
               Create account
             </button>
@@ -94,5 +129,5 @@ export default function SignUp() {
         </div>
       </Modal>
     </>
-  )
-};
+  );
+}
